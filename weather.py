0d27b0f091a736822e172dba65eb974f453f5344
Ed <4ubais04@gmail.com>
2022-09-18 22:37:09 +0300

    Добавлены данные для JSON

--- weather.py
+++ weather.py
@@ -10,7 +10,13 @@ URL = f'https://api.openweathermap.org/data/2.5/weather?q={city},ru&APPID={APPID
 
 def download_weather():
     response = requests.get(url=URL).json()
-    json_str = response['name'] + ': ' + str(response['main']['temp'])
+    name = response['name']                   # название города
+    temperature = response['main']['temp']    # температура
+    pressure = response['main']['pressure']   # АТ давление
+    humidity = response['main']['humidity']   # влажность воздуха
+    rain = response['rain']['1h']             # объем дождя за последний час
+    json_str = f'city = {name}; temperature = {temperature} °C; pressure = {pressure} мм.рт.ст.; ' \
+               f'humidity = {humidity} %; rain = {rain} мм.'
 
     with open('weather_json.txt', 'w') as f:
         json.dump(json_str, f)

5d19b650489fab1ae9eb8fbeede57f5355de9a27
Ed <4ubais04@gmail.com>
2022-09-19 10:00:13 +0300

    убрал 'rain', больше нет такого ключа

--- weather.py
+++ weather.py
@@ -3,20 +3,17 @@ import json
 
 
 APPID = '4e9a0812c9fea74873e7a091fba8d505'
-city = 'Cheboksary'
-units = 'metric' # температура по Цельсию
-URL = f'https://api.openweathermap.org/data/2.5/weather?q={city},ru&APPID={APPID}&units={units}'
+URL = f'https://api.openweathermap.org/data/2.5/weather?q=Cheboksary,ru&APPID={APPID}&units=metric'
 
 
 def download_weather():
     response = requests.get(url=URL).json()
-    name = response['name']                   # название города
-    temperature = response['main']['temp']    # температура
-    pressure = response['main']['pressure']   # АТ давление
-    humidity = response['main']['humidity']   # влажность воздуха
-    rain = response['rain']['1h']             # объем дождя за последний час
-    json_str = f'city = {name}; temperature = {temperature} °C; pressure = {pressure} мм.рт.ст.; ' \
-               f'humidity = {humidity} %; rain = {rain} мм.'
+    name = response['name']                     # название города
+    temperature = response['main']['temp']      # температура
+    pressure = response['main']['pressure']     # АТ давление
+    humidity = response['main']['humidity']     # влажность воздуха
+    json_str = f'city = {name}; temperature = {temperature} °C; ' \
+               f'pressure = {pressure} мм.рт.ст.; humidity = {humidity} %'
 
     with open('weather_json.txt', 'w') as f:
         json.dump(json_str, f)

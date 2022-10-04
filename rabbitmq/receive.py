5d3a58282317c409ffb00eab48ca32fa6746b21a
Ed <4ubais04@gmail.com>
2022-10-04 20:28:49 +0300

    add 'sand.py' and 'receive.py'

---
+++ rabbitmq/receive.py
@@ -0,0 +1,113 @@
+import pika
+import sys
+import os
+import requests
+import psycopg2
+import json
+from psycopg2 import OperationalError
+from datetime import datetime
+
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    channel.basic_consume(queue='hello', on_message_callback=do_work, auto_ack=True)
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+def download():
+    import key_appid as key  # импорт файла с ключом
+    key_appid = key.key_appid
+    url = f'https://api.openweathermap.org/data/2.5/weather?q=Cheboksary,ru&APPID={key_appid}&units=metric'
+    r = requests.get(url=url).json()
+    date_downloads = datetime.today().strftime("%Y-%m-%d")
+    time_downloads = datetime.today().strftime("%H:%M:%S")
+
+    with open('weather_city.json', 'w') as filename:
+        json.dump(r, filename)
+    print("Файл успешно скачан")
+
+    return date_downloads, time_downloads, r
+
+
+def create_connection_db():
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      password="postgres",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="postgres")
+        print("Подключение к базе PostgreSQL выполнено")
+        cursor = connection.cursor()
+        create_table_query = '''CREATE TABLE IF NOT EXISTS public.weather (
+                        id_weather int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+                        date_downloads date,
+                        time_downloads time,
+                        coord json,
+                        weather json,
+                        base text,
+                        main json,
+                        visibility text,
+                        wind json,
+                        clouds json,
+                        dt int,
+                        sys json,
+                        timezone int,
+                        id int,
+                        name text,
+                        cod int
+                        ); '''
+
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Таблица успешно создана")
+    except OperationalError as e:
+        print(f"Произошла ошибка {e}")
+
+
+def insert_json_db(date_downloads, time_downloads, r):
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      password="postgres",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="postgres")
+        print("Подключение к базе PostgreSQL для добавления json выполнено")
+        cursor = connection.cursor()
+        cursor.execute(
+            "INSERT INTO public.weather (date_downloads,time_downloads,coord,weather,base,main,visibility,wind,clouds,dt,sys,timezone,id,name,cod)"
+            "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+            (date_downloads, time_downloads,
+             json.dumps(r["coord"]), json.dumps(r["weather"]), r["base"], json.dumps(r["main"]), r["visibility"],
+             json.dumps(r["wind"]),
+             json.dumps(r["clouds"]), r["dt"], json.dumps(r["sys"]), r["timezone"], r["id"], r["name"], r["cod"]))
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Запись успешно вставлена в таблицу")
+    except (Exception, psycopg2.Error) as error:
+        print("Не удалось вставить данные в таблицу", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Соединение с PostgreSQL закрыто")
+
+
+def do_work():
+    create_connection_db()
+    date, time, resp = download()
+    insert_json_db(date, time, resp)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os.exit(0)
---
+++ rabbitmq/send.py
@@ -0,0 +1,14 @@
+import pika
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='connection_db', durable=True)
+channel.basic_publish(
+    exchange='',
+    routing_key='connection',
+    body='conn_db_dwn_json',
+    properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE)   # очередь задач не потеряется при перезапуске кролика
+    )
+print("Sent message")
+connection.close()
